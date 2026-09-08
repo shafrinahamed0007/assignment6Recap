d124ac23ad3d09999db88b1b60d697fd29bf735a
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-09-09 00:43:24 +0600

    Design Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,95 @@
+const Navbar = () => {
+  return (
+   <div>
+     <div className="navbar bg-base-100 shadow-sm px-[100px]">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              aria-label="Menu"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={-1}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+             <li>
+            <a>Products</a>
+          </li>
+          <li>
+            <a>Features</a>
+          </li>
+          <li>
+            <a>Pricing</a>
+          </li>
+          <li>
+            <a>Testimonials</a>
+          </li>
+          <li>
+            <a>FAQ</a>
+          </li>
+          </ul>
+        </div>
+        <a className="text-xl font-semibold bg-linear-to-r from-[#4f39f6] to-[#9514fa] bg-clip-text text-transparent">DigiTools</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-[16px] font-semibold text-[#101727]">
+          <li>
+            <a>Products</a>
+          </li>
+          <li>
+            <a>Features</a>
+          </li>
+          <li>
+            <a>Pricing</a>
+          </li>
+          <li>
+            <a>Testimonials</a>
+          </li>
+          <li>
+            <a>FAQ</a>
+          </li>
+          
+        </ul>
+      </div>
+      <div className="navbar-end md:flex gap-4">
+        <div className="indicator">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            {" "}
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+            />{" "}
+          </svg>
+          <span class="badge badge-sm indicator-item">0</span>
+        </div>
+        <h2>Login</h2>
+        <button className="btn px-4 py-3 w-[121px] h-[44px] rounded-[90px]  text-white bg-linear-to-r from-[#4f39f6] to-[#9514fa]">Get Started</button>
+      </div>
+    </div>
+   </div>
+  );
+};
+
+export default Navbar;
